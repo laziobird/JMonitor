157091769ab97ef713607f8ee73cdc529ed1b844
laziobird <laziobird@bogon>
2017-04-13 18:23:44 +0800

    继续优化，一些常量

--- src/main/java/monitor/JMonitorTimer.java
+++ src/main/java/monitor/JMonitorTimer.java
@@ -1,33 +1,28 @@
 package monitor;
 
-import monitor.task.JMonitorTask;
-
-import java.util.Date;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-
+import monitor.task.JMonitorTask;
 
 public class JMonitorTimer {
-	  static ScheduledExecutorService scheduExec;
+	static ScheduledExecutorService scheduExec;
+	static final int INTERVAL = 60;
 
-	  public static void load()
-	  {
-	    try {
+	public static void load() {
+		try {
 			scheduExec = Executors.newScheduledThreadPool(1);
-		    JMonitorTask rdtask = new JMonitorTask();
-		    // 测试环境3600*24秒
-		    scheduExec.scheduleWithFixedDelay(rdtask, 5L, 3600*24, TimeUnit.SECONDS);
-		    System.out.println(" timer is work start ,interval : " +3600*24);	
+			JMonitorTask rdtask = new JMonitorTask();
+			// 测试环境3600*24秒
+			scheduExec.scheduleWithFixedDelay(rdtask, 5L, INTERVAL, TimeUnit.SECONDS);
+			System.out.println(" timer is work start ,interval : " + INTERVAL);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
-	  }
+	}
 
-	
 	public static void main(String[] args) {
 	}
-	  
 
 }
